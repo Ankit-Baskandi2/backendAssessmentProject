3b7434705de7e06a6d27e3b7b8b3336746b26e2e
Ankit-Baskandi2 <ankitbaskandi@gmail.com>
2024-07-22 18:52:20 +0530

    first commit

---
+++ AssessementProjectForAddingUser.Domain/Entity/GenderEnumModal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessementProjectForAddingUser.Domain.Entity
+{
+    public enum GenderEnumModal
+    {
+        Male = 0,
+        Femail = 1,
+        Other = 2,
+    }
+}
---
+++ AssessementProjectForAddingUser.Domain/Entity/UserDetailsModal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AssessementProjectForAddingUser.Domain.Entity
+{
+    [Table("userDetails_Ankit")]
+    public class UserDetailsModal
+    {
+        public BigInteger UserId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public string? Gender { get; set; }
+
+        public DateOnly DateOfjoining { get; set; }
+
+        public DateOnly Dob {  get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? AlternatePhone { get; set; }
+
+        public string? EmailPath { get; set; }
+    }
+}
